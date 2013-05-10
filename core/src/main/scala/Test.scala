b80f18061d6990521ab9076a597e5a7e9382c9c6
Jason Zaugg <jzaugg@gmail.com>
2013-05-10 09:05:03 +0200

    Test for the elvis macro.
    The idea is to emulate the Groovy's elvis operator.
    
    elvis(a.b.c) should return null if `a` or `a.b` returns null.

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -1,3 +1,20 @@
 object Test extends App {
   Macros.hello
+
+  object O {
+    class C {
+      class D {
+        def e = "e"
+      }
+      def dNull: D = null
+      def d = new D
+    }
+    def cNull: C = null
+    def c = new C
+  }
+  val e: String = Macros.elvis(O.c.d.e)
+  val e1: String = Macros.elvis(O.cNull.d.e)
+  println(e1)
+  val e2: String = Macros.elvis(O.c.dNull.e)
+  println(e2)
 }
